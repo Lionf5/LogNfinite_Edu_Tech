61352deed309b0ffd4a1fd6def72314eb5240164
AJAYKUMAR  NADAKUDITI <94443955+Lionf5@users.noreply.github.com>
2025-08-08 03:37:31 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
   const socialLinks = [
     { icon: <Facebook className="w-5 h-5" />, href: "#", name: "Facebook" },
     { icon: <Twitter className="w-5 h-5" />, href: "#", name: "Twitter" },
-    { icon: <LinkedIn className="w-5 h-5" />, href: "#", name: "LinkedIn" },
+    { icon: <LinkedIn className="w-5 h-5" />, href: "https://www.linkedin.com/company/code-with-techy/", name: "LinkedIn" },
     { icon: <Instagram className="w-5 h-5" />, href: "#", name: "Instagram" },
     { icon: <Youtube className="w-5 h-5" />, href: "#", name: "YouTube" }
   ];
